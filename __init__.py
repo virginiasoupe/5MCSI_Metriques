a50af368bb48bf6ba1c68be3df5ab4a4ffb43198
virginiasoupe <159542972+virginiasoupe@users.noreply.github.com>
2025-04-01 14:14:00 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
-app = Flask(__name__)                                                                                                                  
+app = Flask(__name__) #commit                                                                                                                 
                                                                                                                                        
 @app.route('/')
 def hello_world():
